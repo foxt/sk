6694806c5021b9b7c74cd7398fc24d7930fb81ca
foxt <me@foxt.dev>
2023-11-03 15:21:06 +0000

    secret store

---
+++ sk.UI.Mac/SecretStore/UserDefaultsSecretStore.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using sk.Core;
+using Security;
+using Foundation;
+namespace sk.UI.Mac {
+    public class PreferencesSecretStore : BaseSecretStore {
+
+
+        override public string? GetSecret(string service) {
+            return NSUserDefaults.StandardUserDefaults.StringForKey("token-" + service);
+        }
+        override public bool SetSecret(string service, string secret, string username) {
+            NSUserDefaults.StandardUserDefaults.SetString(secret, "token-" + service);
+            return true;
+        }
+    }
+}
+
